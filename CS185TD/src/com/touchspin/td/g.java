1c044d4c84eea74dc2f62c1811905989ea7c81b3
rambudo <rambudo@gmail.com>
2014-05-15 17:00:44 -0700

    Some changes to g.

--- CS185TD/src/com/touchspin/td/g.java
+++ CS185TD/src/com/touchspin/td/g.java
@@ -4,15 +4,25 @@ import com.badlogic.gdx.Gdx;
 
 public class g {
 	
-	public int level;
-	public boolean sfx;
-	public boolean music;
-	public int sfxLevel;
-	public int musicLevel;
-	public String language;
-	public char controls;
-	public float accelX;
-	public float accelY;
+	// Player variables
+	public int level; 		// Progress counter
+	
+	// Sound variables
+	public boolean sfx;		// Sound effects are activated
+	public boolean music;	// Music is activated
+	public int sfxLevel;	// Sound effect volume. 0=None, 100=Full
+	public int musicLevel;	// Music volume. 0=None, 100=Full
+	
+	// System variables
+	public String language;	// Language used. This is a to letter code indicating language
+							// en=English.
+	public char controls;	// Control set being used. A=Android, K=Keyboard, M=Mouse
+							// D=Desktop (Mouse + Keyboard)
+	
+	// Background variables
+	public float accelX;	// Support for accelerometer X axis
+	public float accelY;	// Support for accelerometer Y axis
+	public InputAnonymizer InputAnonymizer = new InputAnonymizer();
 	
 	private static g singleton = new g( );
 	
@@ -22,7 +32,11 @@ public class g {
 		music = true;
 		sfxLevel = 100;
 		musicLevel = 100;
-		language = "EN";
+		language = java.util.Locale.getDefault().getLanguage();
+		if (language=="en");
+		else if (language=="")
+			language="en";
+		else language="en";
 		switch (Gdx.app.getType()){
 			case Android:
 				controls = 'A';
