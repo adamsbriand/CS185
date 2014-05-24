3133b09708e5278584a7c877a9091d66d685aca4
Wendi <810852915@qq.com>
2014-05-23 18:04:03 -0700

    The fire sprite now rotates based on the speed of the ball. It looks kind of wired in the runner game. It looks better in maze.

--- CS185TD/src/com/touchspin/td/Hero.java
+++ CS185TD/src/com/touchspin/td/Hero.java
@@ -11,10 +11,10 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
-public  class Hero extends GameThing {
+public class Hero extends GameThing {
 	public OrthographicCamera camera;
 	public static MoverInput heroMover = new MoverInput();
-	
+
 	private Texture appearance;
 	private Texture fire;
 	private Animation fireAnimation;
@@ -23,100 +23,158 @@ public  class Hero extends GameThing {
 	private int frameCount = 0;
 	public Sprite heroSprite;
 	public Sprite fireEffect;
-	private float stateTime; 
+	private float stateTime;
 	public boolean fireOn;
-	//private float distancePerFrameX;
-	//private float distancePerFrameY;
-	//private int gravity = -10;	
-		
 
-	public Hero(OrthographicCamera camera,TiledMapWrapper tiledMapWrapper) {
+	// private float distancePerFrameX;
+	// private float distancePerFrameY;
+	// private int gravity = -10;
+
+	public Hero(OrthographicCamera camera, TiledMapWrapper tiledMapWrapper) {
 		this.tiledMapWrapper = tiledMapWrapper;
 		this.camera = camera;
-		
-		appearance = new Texture(Gdx.files.internal("data/Ball100BallBearingBrass.png"));
+
+		appearance = new Texture(
+				Gdx.files.internal("data/Ball100BallBearingBrass.png"));
 		heroSprite = new Sprite(appearance);
 		heroSprite.setBounds(0, 32, 32 * camera.zoom, 32 * camera.zoom);
-		heroSprite.setOrigin(heroSprite.getWidth()/2, heroSprite.getHeight()/2);
+		heroSprite.setOrigin(heroSprite.getWidth() / 2,
+				heroSprite.getHeight() / 2);
 		setHeight(heroSprite.getHeight() * camera.zoom);
 		setWidth(heroSprite.getWidth() * camera.zoom);
 		setX(10);
 		setY(100);
-		
-		//read in file animation
-		fire = new Texture(Gdx.files.internal("data/Fireball_Frame100x240_Sheet_1200x960_RC12x4_Frames48.png"));
-        TextureRegion[][] tmp = TextureRegion.split(fire, fire.getWidth()/12, fire.getHeight()/4);  
-        fireFrames = new TextureRegion[12*4];
-        int index = 0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 12; j++) {
-                fireFrames[index++] = tmp[i][j];
-            }
-        }
-        fireAnimation = new Animation(0.025f, fireFrames); 
-        stateTime = 0f;
-        fireOn = true;
-        currentFrame = fireAnimation.getKeyFrame(stateTime, true);	
-        
-		fireEffect = new Sprite(currentFrame);
-		fireEffect.setBounds(0, 32, 32 * camera.zoom, 32 *fireEffect.getHeight()/fireEffect.getWidth()* camera.zoom);
-		fireEffect.setOrigin(heroSprite.getWidth()/2, heroSprite.getHeight()/2);
 
+		// read in file animation
+		fire = new Texture(
+				Gdx.files
+						.internal("data/Fireball_Frame100x240_Sheet_1200x960_RC12x4_Frames48.png"));
+		TextureRegion[][] tmp = TextureRegion.split(fire, fire.getWidth() / 12,
+				fire.getHeight() / 4);
+		fireFrames = new TextureRegion[12 * 4];
+		int index = 0;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 12; j++) {
+				fireFrames[index++] = tmp[i][j];
+			}
+		}
+		fireAnimation = new Animation(0.025f, fireFrames);
+		stateTime = 0f;
+		fireOn = true;
+		currentFrame = fireAnimation.getKeyFrame(stateTime, true);
+
+		fireEffect = new Sprite(currentFrame);
+		fireEffect.setBounds(0, 32, 32 * camera.zoom,
+				32 * fireEffect.getHeight() / fireEffect.getWidth()
+						* camera.zoom);
+		fireEffect.setOrigin(heroSprite.getWidth() / 2,
+				heroSprite.getHeight() / 2);
 
 	}
 
 	@Override
 	public void draw(Batch batch, float alpha) {
 		batch.setProjectionMatrix(camera.combined);
-		if(frameCount == 15)
-		{
+		if (frameCount == 15) {
 			heroSprite.setColor(Color.RED);
-		}
-		else if(frameCount == 1)
-		{
+		} else if (frameCount == 1) {
 			heroSprite.setColor(Color.WHITE);
 		}
 		heroSprite.draw(batch);
-		if(fireOn)
-		{
+		if (fireOn) {
 			drawFireEffect(batch);
 		}
 	}
 
-	private void drawFireEffect(Batch batch)
-	{
-		//batch.draw(currentFrame,getX(),getY(),32f,currentFrame.getRegionHeight()*32/currentFrame.getRegionWidth());
+	private void drawFireEffect(Batch batch) {
+		// batch.draw(currentFrame,getX(),getY(),32f,currentFrame.getRegionHeight()*32/currentFrame.getRegionWidth());
 		fireEffect.draw(batch);
 	}
+
 	@Override
 	public void act(float delta) {
 		heroMover.move(this);
-		
-		//Attack
+
+		// Attack
 		if (frameCount > 1)
 			frameCount--;
-		if(g.i().leAnonymizer.attack)
-		{
+		if (g.i().leAnonymizer.attack) {
 			attack();
 			g.i().leAnonymizer.attack = false;
 		}
-		//position
+		// position
 		heroSprite.setX(getX());
 		heroSprite.setY(getY());
 		fireEffect.setX(getX());
 		fireEffect.setY(getY());
-		
-		//Rotation
-		heroSprite.rotate(360*(heroMover.previousX - getX())/((float)Math.PI * heroSprite.getRegionHeight()));
-		
-		//Fire animation
+
+		// Rotation
+		heroSprite.rotate(360 * (heroMover.previousX - getX())
+				/ ((float) Math.PI * heroSprite.getRegionHeight()));
+		setFireRotation();
+
+		// Fire animation
 		stateTime += Gdx.graphics.getDeltaTime();
 		currentFrame = fireAnimation.getKeyFrame(stateTime, true);
 		fireEffect.setRegion(currentFrame);
-		
+
 	}
 
 	public void attack() {
 		frameCount = 15;
 	}
+
+	private void setFireRotation() {
+
+		if (heroMover.speedXPerSecond == 0) {
+			if (heroMover.speedYPerSecond > 0)
+				fireEffect.setRotation((float) (-180));
+			else if (heroMover.speedYPerSecond < 0)
+				fireEffect.setRotation(0);
+		}
+
+		else if (heroMover.speedYPerSecond == 0) {
+
+			if (g.i().gameMode == 'R') {
+				if (heroMover.speedXPerSecond > 0)
+					fireEffect.setRotation((float) (40 * Math
+							.atan(heroMover.speedXPerSecond / 200)));
+				else if (heroMover.speedXPerSecond < 0)
+					fireEffect.setRotation((float) (40 * Math
+							.atan(heroMover.speedXPerSecond / 200)));
+			} else {
+				if (heroMover.speedXPerSecond > 0)
+					fireEffect.setRotation((float) (90));
+				else if (heroMover.speedXPerSecond < 0)
+					fireEffect.setRotation((float) (-90));
+			}
+		}
+
+		else if (heroMover.speedXPerSecond * heroMover.speedYPerSecond > 0) {
+			if (heroMover.speedXPerSecond > 0)
+				fireEffect.setRotation((float) (90 + Math
+						.atan(heroMover.speedYPerSecond
+								/ heroMover.speedXPerSecond)
+						/ Math.PI * 180));
+			else
+				fireEffect.setRotation((float) (-(90 - Math
+						.atan(heroMover.speedYPerSecond
+								/ heroMover.speedXPerSecond)
+						/ Math.PI * 180)));
+
+		} else if (heroMover.speedXPerSecond * heroMover.speedYPerSecond < 0) {
+
+			if (heroMover.speedXPerSecond > 0)
+				fireEffect.setRotation((float) (90 + Math
+						.atan(heroMover.speedYPerSecond
+								/ heroMover.speedXPerSecond)
+						/ Math.PI * 180));
+			else
+				fireEffect.setRotation((float) (-90 + Math
+						.atan(heroMover.speedYPerSecond
+								/ heroMover.speedXPerSecond)
+						/ Math.PI * 180));
+		}
+	}
+
 }
