53ebd333381610fe84c25045cf72f94f06a9df66
Wendi <810852915@qq.com>
2014-06-04 22:23:43 -0700

    Add teleport sound effect.

--- CS185TD/src/com/touchspin/td/Sounds.java
+++ CS185TD/src/com/touchspin/td/Sounds.java
@@ -25,6 +25,8 @@ public class Sounds {
 	private static final String bounce2TennisBall = "snd/sndTennisBallBounce2.wav";
 	private static final String bounce3TennisBall = "snd/sndTennisBallBounce3.wav";
 	private static final String splash = "snd/sndSplash.wav";
+	private static final String Teleport = "snd/Teleport.wav";
+	
 	// Sound effects
 	private static final String arrowfile1 = "snd/sndArrow1.wav";
 	private static final String arrowfile2 = "snd/sndArrow2.wav";
@@ -241,8 +243,13 @@ public class Sounds {
 				break;
 			case "brick":
 				sndSwitch=Gdx.audio.newSound(Gdx.files.internal(brickCrash));
+				break;
 			case "splash":
 				sndSwitch=Gdx.audio.newSound(Gdx.files.internal(splash));
+				break;
+			case "Teleport":
+				sndSwitch=Gdx.audio.newSound(Gdx.files.internal(Teleport));
+				break;
 		}
 		sndSwitch.play(g.i().sfxLevel);
 	}
