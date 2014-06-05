aaf2f6df3f73877a56cee40a356ecb1e60e61cbd
rambudo <rambudo@gmail.com>
2014-06-04 23:37:35 -0700

    Fix merging error.

--- CS185TD/src/com/touchspin/td/TiledMapWrapper.java
+++ CS185TD/src/com/touchspin/td/TiledMapWrapper.java
@@ -167,20 +167,6 @@ public class TiledMapWrapper {
 					.getObjects();
 		
 //		if (tiledMap.getLayers().get("blocks") != null) {
-<<<<<<< HEAD
-//		temp = (TiledMapTileLayer) tiledMap.getLayers().get("blocks");
-//		tileWidth = temp.getTileWidth();
-//		tileHeight = temp.getTileHeight();
-//		for (int i = 0; i < temp.getWidth(); i++)
-//			for (int j = 0; j < temp.getHeight(); j++) {
-//				if (temp.getCell(i, j) != null) {
-//					tempObj = new RectangleMapObject(i * tileWidth, j
-//							* tileHeight, tileWidth, tileHeight);
-//					collisionObjects.add(tempObj);
-//				}
-//			}
-//	}
-=======
 //			temp = (TiledMapTileLayer) tiledMap.getLayers().get("blocks");
 //			tileWidth = temp.getTileWidth();
 //			tileHeight = temp.getTileHeight();
@@ -193,7 +179,6 @@ public class TiledMapWrapper {
 //					}
 //				}
 //		}
->>>>>>> refs/remotes/origin/master
 
 		if (tiledMap.getLayers().get("collisions") != null) {
 			temp = (TiledMapTileLayer) tiledMap.getLayers().get("collisions");
