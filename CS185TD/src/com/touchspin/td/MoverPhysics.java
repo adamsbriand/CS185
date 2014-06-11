87b1dd513cc6a5b2003ed3f18a43dd77971a0b9f
Damian <dforrester7777@gmail.com>
2014-06-11 12:43:15 -0700

    Collision in physics mover fixed

--- CS185TD/src/com/touchspin/td/MoverPhysics.java
+++ CS185TD/src/com/touchspin/td/MoverPhysics.java
@@ -22,7 +22,7 @@ public class MoverPhysics extends Mover {
 	HashSet<NP> triggeredNP = new HashSet<NP>();
 	protected NP water;
 	boolean isInWater;
-
+	
 	public MoverPhysics() {
 		speedXPerSecond = 0;
 		speedYPerSecond = 0;
@@ -97,88 +97,96 @@ public class MoverPhysics extends Mover {
 	protected boolean isXFree() {
 		if (speedXPerSecond < 0) // going to the left
 		{
-			if (gameThing.getX() < 0) // collide with left edge of map
-				return false;
-
-			// check object collision
-			circleCenter = new Vector2(gameThing.getX() + radius,
-					gameThing.getY() + radius);
-			// iterate through objects
-			for (MapObject object : gameThing.tiledMapWrapper.collisionObjects) {
-				if (object instanceof RectangleMapObject) {
-					temp = (RectangleMapObject) object;
-					rect = temp.getRectangle();
-
-					// check if object is not to the left of the player
-					// or if the object is more than 1 tile away from the player
-					if (rect.x > (gameThing.getX() + gameThing.getWidth())
-							|| gameThing.getX() - (rect.x + rect.width) > 2 * tileWidth)
-						continue;
-
-					// iterate through y values of object
-					for (int countY = (int) rect.y; countY < (rect.y + rect.height); countY++) {
-						// player collides with object
-						if (circleCenter.dst(rect.x + rect.width, countY) < radius) {
-							if (g.i().npMap.get(object) != null) {
-								triggeredNP.add(g.i().npMap.get(object));
-								if (g.i().npMap.get(object).collidable) {
+			// unit for loop			
+			for(float currentX = previousX; currentX > gameThing.getX(); currentX--)
+			{
+				if (currentX < 0) // collide with left edge of map
+					return false;
+	
+				// check object collision
+				circleCenter = new Vector2(currentX + radius,
+						gameThing.getY() + radius);
+				// iterate through objects
+				for (MapObject object : gameThing.tiledMapWrapper.collisionObjects) {
+					if (object instanceof RectangleMapObject) {
+						temp = (RectangleMapObject) object;
+						rect = temp.getRectangle();
+	
+						// check if object is not to the left of the player
+						// or if the object is more than 1 tile away from the player
+						if (rect.x > (currentX + gameThing.getWidth())
+								|| currentX - (rect.x + rect.width) > 2 * tileWidth)
+							continue;
+	
+						// iterate through y values of object
+						for (int countY = (int) rect.y; countY < (rect.y + rect.height); countY++) {
+							// player collides with object
+							if (circleCenter.dst(rect.x + rect.width, countY) < radius) {
+								if (g.i().npMap.get(object) != null) {
+									triggeredNP.add(g.i().npMap.get(object));
+									if (g.i().npMap.get(object).collidable) {
+										previousX = rect.x + rect.width;
+										return false;
+									}
+								} else {
 									previousX = rect.x + rect.width;
 									return false;
 								}
-							} else {
-								previousX = rect.x + rect.width;
-								return false;
 							}
 						}
-					}
-
-				}// end of if object is a rectanlgeMapObject
-			}// end of for object iterator
+	
+					}// end of if object is a rectanlgeMapObject
+				}// end of for object iterator
+			}// end of unit for loop			
 		}// end of going to the left check
 
 		else if (speedXPerSecond > 0) // going to the right
 		{
-			// collide with right edge of the map
-			if (gameThing.getX() + gameThing.getWidth() > gameThing.tiledMapWrapper
-					.getPixelWidth())
-				return false;
-
-			// check object collision
-			circleCenter = new Vector2(gameThing.getX() + radius,
-					gameThing.getY() + radius);
-			// iterate through objects
-			for (MapObject object : gameThing.tiledMapWrapper.collisionObjects) {
-				if (object instanceof RectangleMapObject) {
-					temp = (RectangleMapObject) object;
-					rect = temp.getRectangle();
-
-					// check if object is not to the right of the player
-					// or if the object is more than 1 tile away from the player
-					if (rect.x < gameThing.getX()
-							|| rect.x
-									- (gameThing.getX() + gameThing.getWidth()) > 2 * tileWidth)
-						continue;
-
-					// iterate through y values of object
-					for (int countY = (int) rect.y; countY < (rect.y + rect.height); countY++) {
-						// player collides with object
-						if (circleCenter.dst(rect.x, countY) < radius) {
-
-							if (g.i().npMap.get(object) != null) {
-								triggeredNP.add(g.i().npMap.get(object));
-								if (g.i().npMap.get(object).collidable) {
+			//unit for loop
+			for(float currentX = previousX; currentX < gameThing.getX(); currentX++)
+			{
+				// collide with right edge of the map
+				if (currentX + gameThing.getWidth() > gameThing.tiledMapWrapper
+						.getPixelWidth())
+					return false;
+	
+				// check object collision
+				circleCenter = new Vector2(currentX + radius,
+						gameThing.getY() + radius);
+				// iterate through objects
+				for (MapObject object : gameThing.tiledMapWrapper.collisionObjects) {
+					if (object instanceof RectangleMapObject) {
+						temp = (RectangleMapObject) object;
+						rect = temp.getRectangle();
+	
+						// check if object is not to the right of the player
+						// or if the object is more than 1 tile away from the player
+						if (rect.x < currentX
+								|| rect.x
+										- (currentX + gameThing.getWidth()) > 2 * tileWidth)
+							continue;
+	
+						// iterate through y values of object
+						for (int countY = (int) rect.y; countY < (rect.y + rect.height); countY++) {
+							// player collides with object
+							if (circleCenter.dst(rect.x, countY) < radius) {
+	
+								if (g.i().npMap.get(object) != null) {
+									triggeredNP.add(g.i().npMap.get(object));
+									if (g.i().npMap.get(object).collidable) {
+										previousX = rect.x - gameThing.getWidth();
+										return false;
+									}
+								} else {
 									previousX = rect.x - gameThing.getWidth();
 									return false;
 								}
-							} else {
-								previousX = rect.x - gameThing.getWidth();
-								return false;
 							}
 						}
-					}
-
-				}// end of if object is a rectanlgeMapObject
-			}// end of for object iterator
+	
+					}// end of if object is a rectanlgeMapObject
+				}// end of for object iterator
+			}//end of unit for loop
 		}// end of going to the right check
 		return true;
 	}// end of isXFree()
@@ -186,91 +194,99 @@ public class MoverPhysics extends Mover {
 	protected boolean isYFree() {
 		if (speedYPerSecond < 0) // going down
 		{
-			if (gameThing.getY() < 0) // collide with bottom of map
+			//unit for loop
+			for(float currentY = previousY; currentY > gameThing.getY(); currentY--)
 			{
-				return false;
-			}
-
-			// check object collision
-			circleCenter = new Vector2(gameThing.getX() + radius,
-					gameThing.getY() + radius);
-			// iterate through objects
-			for (MapObject object : gameThing.tiledMapWrapper.collisionObjects) {
-				if (object instanceof RectangleMapObject) {
-					temp = (RectangleMapObject) object;
-					rect = temp.getRectangle();
-
-					// check if object is not to the bottom of the player
-					// or if the object is more than 1 tile away from the player
-					if (rect.y > gameThing.getY()
-							|| gameThing.getY() - (rect.y + rect.height) > 2 * tileHeight)
-						continue;
-
-					// iterate through x values of object
-					for (int countX = (int) rect.x; countX < (rect.x + rect.width); countX++) {
-						// player collides with object
-						if (circleCenter.dst(countX, rect.y + rect.height) < radius) {
-							if (g.i().npMap.get(object) != null) {
-								triggeredNP.add(g.i().npMap.get(object));
-								if (g.i().npMap.get(object).collidable) {
+				if (currentY < 0) // collide with bottom of map
+				{
+					return false;
+				}
+	
+				// check object collision
+				circleCenter = new Vector2(gameThing.getX() + radius,
+						currentY + radius);
+				// iterate through objects
+				for (MapObject object : gameThing.tiledMapWrapper.collisionObjects) {
+					if (object instanceof RectangleMapObject) {
+						temp = (RectangleMapObject) object;
+						rect = temp.getRectangle();
+	
+						// check if object is not to the bottom of the player
+						// or if the object is more than 1 tile away from the player
+						if (rect.y > currentY
+								|| currentY - (rect.y + rect.height) > 2 * tileHeight)
+							continue;
+	
+						// iterate through x values of object
+						for (int countX = (int) rect.x; countX < (rect.x + rect.width); countX++) {
+							// player collides with object
+							if (circleCenter.dst(countX, rect.y + rect.height) < radius) {
+								if (g.i().npMap.get(object) != null) {
+									triggeredNP.add(g.i().npMap.get(object));
+									if (g.i().npMap.get(object).collidable) {
+										previousY = rect.y + rect.height;
+										return false;
+									}
+								} else {
 									previousY = rect.y + rect.height;
 									return false;
 								}
-							} else {
-								previousY = rect.y + rect.height;
-								return false;
 							}
+	
 						}
-
-					}
-
-				}// end of if object is a rectanlgeMapObject
-			}// end of for object iterator
+	
+					}// end of if object is a rectanlgeMapObject
+				}// end of for object iterator
+			}// end of unit for loop
 		}// end of going down check
 
 		else if (speedYPerSecond > 0) // going up
 		{
-			// collide with top of the map
-			if (gameThing.getY() + gameThing.getHeight() > gameThing.tiledMapWrapper
-					.getPixelHeight())
-				return false;
-
-			// check object collision
-			circleCenter = new Vector2(gameThing.getX() + radius,
-					gameThing.getY() + radius);
-			// iterate through objects
-			for (MapObject object : gameThing.tiledMapWrapper.collisionObjects) {
-				if (object instanceof RectangleMapObject) {
-					temp = (RectangleMapObject) object;
-					rect = temp.getRectangle();
-
-					// check if object is not above the player
-					// or if the object is more than 1 tile away from the player
-					if (rect.y < gameThing.getY()
-							|| rect.y
-									- (gameThing.getY() + gameThing.getHeight()) > 2 * tileHeight)
-						continue;
-
-					// iterate through y values of object
-					for (int countX = (int) rect.x; countX < (rect.x + rect.width); countX++) {
-						// player collides with object
-						if (circleCenter.dst(countX, rect.y) < radius) {
-
-							if (g.i().npMap.get(object) != null) {
-								triggeredNP.add(g.i().npMap.get(object));
-								if (g.i().npMap.get(object).collidable) {
+			//unit for loop
+			for(float currentY = previousY; currentY < gameThing.getY(); currentY++)
+			{
+				// collide with top of the map
+				if (currentY + gameThing.getHeight() > gameThing.tiledMapWrapper
+						.getPixelHeight())
+					return false;
+	
+				// check object collision
+				circleCenter = new Vector2(gameThing.getX() + radius,
+						currentY + radius);
+				// iterate through objects
+				for (MapObject object : gameThing.tiledMapWrapper.collisionObjects) {
+					if (object instanceof RectangleMapObject) {
+						temp = (RectangleMapObject) object;
+						rect = temp.getRectangle();
+	
+						// check if object is not above the player
+						// or if the object is more than 1 tile away from the player
+						if (rect.y < currentY
+								|| rect.y
+										- (currentY + gameThing.getHeight()) > 2 * tileHeight)
+							continue;
+	
+						// iterate through y values of object
+						for (int countX = (int) rect.x; countX < (rect.x + rect.width); countX++) {
+							// player collides with object
+							if (circleCenter.dst(countX, rect.y) < radius) {
+	
+								if (g.i().npMap.get(object) != null) {
+									triggeredNP.add(g.i().npMap.get(object));
+									if (g.i().npMap.get(object).collidable) {
+										previousY = rect.y - gameThing.getHeight();
+										return false;
+									}
+								} else {
 									previousY = rect.y - gameThing.getHeight();
 									return false;
 								}
-							} else {
-								previousY = rect.y - gameThing.getHeight();
-								return false;
 							}
 						}
-					}
-
-				}// end of if object is a rectanlgeMapObject
-			}// end of for object iterator
+	
+					}// end of if object is a rectanlgeMapObject
+				}// end of for object iterator
+			}//end of unit for loop
 		}// end of going up
 		return true;
 	}// end of isYFree()
