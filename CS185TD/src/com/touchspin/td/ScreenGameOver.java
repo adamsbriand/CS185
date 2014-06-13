a67ba24192b10522054339037c762a064ae1d5d0
rambudo <rambudo@gmail.com>
2014-06-13 13:23:53 -0700

    Cleaned up code and added constants in ScreenGameOver

--- CS185TD/src/com/touchspin/td/ScreenGameOver.java
+++ CS185TD/src/com/touchspin/td/ScreenGameOver.java
@@ -10,6 +10,12 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.TimeUtils;
 
 public class ScreenGameOver extends GameMenu{
+	
+	// Constants
+	private static final String bgImage = "img/menu/TeamOllie1200.png";
+	private static final String logoImage = "img/menu/LogoGameOver.png";
+	
+	// Variables
 	long timeStartGame;
 	
 	public ScreenGameOver(MainGame MainGame){
@@ -24,7 +30,7 @@ public class ScreenGameOver extends GameMenu{
 	
 	void setBG(){
 		camera = new OrthographicCamera();
-	    bg = new Sprite(new Texture(Gdx.files.internal("img/menu/TeamOllie1200.png")));
+	    bg = new Sprite(new Texture(Gdx.files.internal(bgImage)));
 	    float ratio = Gdx.graphics.getHeight() / bg.getHeight();
 	    bg.setSize(bg.getWidth() * ratio, bg.getHeight() * ratio);
 		if (bg.getWidth() > Gdx.graphics.getWidth()){
@@ -32,15 +38,15 @@ public class ScreenGameOver extends GameMenu{
 			bg.setSize(bg.getWidth() * ratio, bg.getHeight() * ratio);
 		}
 	    bg.setOrigin(bg.getWidth() / 2, bg.getHeight() / 2);
-	    bg.setPosition((Gdx.graphics.getWidth() - bg.getWidth()) / 2, (Gdx.graphics.getHeight() - bg.getHeight()) / 2);
+	    bg.setPosition((Gdx.graphics.getWidth() - bg.getWidth()) / 2, 
+	    		(Gdx.graphics.getHeight() - bg.getHeight()) / 2);
 	}
 
 	void buttons() {
 		final float xRow2 = Gdx.graphics.getHeight() / 15 * 13;
     	final float yRow1_2 = Gdx.graphics.getWidth() / 9 * 7;
-    	float height = 0;
-    	float width = 0;
-    	final TextButton continueButton = new TextButton(g.i().Continue, textButtonStyle);
+    	final TextButton continueButton = 
+    			new TextButton(g.i().Continue, textButtonStyle);
     	continueButton.addListener(new ChangeListener() {
             public void changed (ChangeEvent event, Actor actor) {
             	g.i().t.action("menu,Main");
@@ -48,15 +54,16 @@ public class ScreenGameOver extends GameMenu{
         });
         stage.addActor(continueButton);
         
-        height = continueButton.getHeight() / 2;
-        width = continueButton.getWidth() / 2;
+        float height = continueButton.getHeight() / 2;
+        float width = continueButton.getWidth() / 2;
         
         continueButton.setPosition(yRow1_2 - width, xRow2 - height);
 	}
 
 	void logo() {
-		super.setLogo(Gdx.files.internal("img/menu/LogoGameOver.png"));
-		logo.setPosition(logo.getX(), logo.getY() - logo.getHeight() / 2);
+		super.setLogo(Gdx.files.internal(logoImage));
+		logo.setPosition(logo.getX(), 
+				logo.getY() - logo.getHeight() / 2);
 	}
 	
 	public void update() {
