69e62c51501a6b370ee9d0063e1bfa770ea49cd5
Wendi <810852915@qq.com>
2014-06-15 12:07:34 -0700

    Document AnimationDescription.java class.

--- CS185TD/src/com/touchspin/td/AnimationDescription.java
+++ CS185TD/src/com/touchspin/td/AnimationDescription.java
@@ -1,16 +1,32 @@
 package com.touchspin.td;
 
 public class AnimationDescription {
-
+/* ======================================================================================
+ * AnimationDescription.java
+ * 
+ * This class holds  information about an animation includes name, startFrame, endFrame
+ * frame range and the next animation name after the current animation.
+ * ======================================================================================
+ */
 	public int frameFirst = 0;
 	public int frameLast = 0;
 	public int frameRange = 0;
 	public String name = "";
 	public String nextAnim = "";
 
+/*
+ * Defualt constructor
+ */
 	public AnimationDescription() {
 	}
-
+/**
+ * Overloaded constructor
+ * @param name - the name of current anmiation
+ * @param frameFirst - the index of the region of the 
+ * first frame on the spritesheet
+ * @param frameLast - the index of the region of last frame on the spritesheet
+ * @param nextAnim - the name of the next animation
+ */
 	public AnimationDescription(String name, int frameFirst, int frameLast,
 			String nextAnim) {
 		this.name = name;
