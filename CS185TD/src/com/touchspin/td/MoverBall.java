62778fc9eec7d72f1386c1b6abf20eb4db7a15fb
rambudo <rambudo@gmail.com>
2014-06-15 22:30:56 -0700

    Removed several unused imports.

--- CS185TD/src/com/touchspin/td/MoverBall.java
+++ CS185TD/src/com/touchspin/td/MoverBall.java
@@ -2,10 +2,7 @@ package com.touchspin.td;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.maps.MapObject;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 /* ======================================================================================
